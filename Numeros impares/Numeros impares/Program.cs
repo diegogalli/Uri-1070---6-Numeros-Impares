fee0312bc443a8abd2807b84e11f8b10398f797e
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 15:31:41 -0300

    Uri 1070 6 numeors impares

---
+++ Numeros impares/Numeros impares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Numeros_impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0) // x recebeu o valor 8, dividido por 2 que é igual 0
+            {
+                x = x + 1; // 8 = 8 + 1
+            }
+            
+            Console.WriteLine(x); // Console leu 9 que é o novo valor do X, antes era 8
+            Console.WriteLine(x + 2); // Console leu 9 + 2
+            Console.WriteLine(x + 4); // Console leu 9 + 4
+            Console.WriteLine(x + 6); // Console leu 9 + 6
+            Console.WriteLine(x + 8); // Console leu 9 + 8
+            Console.WriteLine(x + 10); // Console leu 9 + 10
+
+           
+        }
+
+    }
+}
